95e618cebc6806ffc9429f91438b2d7f8bf4412a
Mycroft <kaseyfitton@hotmail.co.uk>
2022-04-22 04:36:50 +0100

    fix typo

--- config.lua
+++ config.lua
@@ -90,7 +90,7 @@ Config.RestrictedChannels = {
 Config.MaxFrequency = 500
 
 Config.messages = {
-    ["not onradio"] = "You're not connected to a signal",
+    ["not on radio"] = "You're not connected to a signal",
     ["on radio"] = "You're already connected to this signal",
     ["joined to radio"] = "You're connected to: ",
     ["restricted channel error"] = "You can not connect to this signal!",
